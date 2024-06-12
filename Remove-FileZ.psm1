b33ef03fc5e728bee1dc7ef53030fe084cb2d73a
zoicware <118035521+zoicware@users.noreply.github.com>
2024-06-12 18:47:54 -0400

    version 0.0.2

--- Remove-FileZ.psm1
+++ Remove-FileZ.psm1
@@ -28,13 +28,12 @@ function Remove-FileZ {
         # Delete all files/folders inside of speech folder but keep directory
 
         .LINK
-       #github repo
+        https://github.com/zoicware/RemoveFileZ
 
         .NOTES
         Author: Zoic
         Twitter: https://twitter.com/1zoic
         GitHub: https://github.com/zoicware
-        PSGallery: 
     #>
 
 
@@ -48,7 +47,7 @@ function Remove-FileZ {
     )
 
 
-
+    <#
     function DownloadHandleApp {
         $Path = "$env:ProgramData\Handle"
         if (!(Test-path $Path)) {
@@ -70,12 +69,161 @@ function Remove-FileZ {
         }
 
     }
+#>
 
 
-
-
-
-
+    # https://github.com/pldmgg/misc-powershell/blob/master/MyFunctions/PowerShellCore_Compatible/Get-FileLockProcess.ps1
+    function Get-FileLockProcess {
+        [CmdletBinding()]
+        Param(
+            [Parameter(Mandatory = $true)]
+            [string[]]
+            $FilePath
+        )
+        $AssembliesFullInfo = [System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object {
+            $_.GetName().Name -eq 'Microsoft.CSharp' -or
+            $_.GetName().Name -eq 'mscorlib' -or
+            $_.GetName().Name -eq 'System' -or
+            $_.GetName().Name -eq 'System.Collections' -or
+            $_.GetName().Name -eq 'System.Core' -or
+            $_.GetName().Name -eq 'System.IO' -or
+            $_.GetName().Name -eq 'System.Linq' -or
+            $_.GetName().Name -eq 'System.Runtime' -or
+            $_.GetName().Name -eq 'System.Runtime.Extensions' -or
+            $_.GetName().Name -eq 'System.Runtime.InteropServices'
+        } | Where-Object { $_.IsDynamic -eq $False }
+        $ReferencedAssemblies = $AssembliesFullInfo.FullName | Sort-Object | Get-Unique
+        $TypeDefinition = @'
+		using Microsoft.CSharp;
+		using System.Collections.Generic;
+		using System.Collections;
+		using System.IO;
+		using System.Linq;
+		using System.Runtime.InteropServices;
+		using System.Runtime;
+		using System;
+		using System.Diagnostics;
+		namespace MyCore.Utils
+		{
+			static public class FileLockUtil
+			{
+				[StructLayout(LayoutKind.Sequential)]
+				struct RM_UNIQUE_PROCESS
+				{
+					public int dwProcessId;
+					public System.Runtime.InteropServices.ComTypes.FILETIME ProcessStartTime;
+				}
+				const int RmRebootReasonNone = 0;
+				const int CCH_RM_MAX_APP_NAME = 255;
+				const int CCH_RM_MAX_SVC_NAME = 63;
+				enum RM_APP_TYPE
+				{
+					RmUnknownApp = 0,
+					RmMainWindow = 1,
+					RmOtherWindow = 2,
+					RmService = 3,
+					RmExplorer = 4,
+					RmConsole = 5,
+					RmCritical = 1000
+				}
+				[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+				struct RM_PROCESS_INFO
+				{
+					public RM_UNIQUE_PROCESS Process;
+					[MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_APP_NAME + 1)]
+					public string strAppName;
+					[MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_SVC_NAME + 1)]
+					public string strServiceShortName;
+					public RM_APP_TYPE ApplicationType;
+					public uint AppStatus;
+					public uint TSSessionId;
+					[MarshalAs(UnmanagedType.Bool)]
+					 public bool bRestartable;
+				}
+				[DllImport("rstrtmgr.dll", CharSet = CharSet.Unicode)]
+				static extern int RmRegisterResources(uint pSessionHandle,
+					UInt32 nFiles,
+					string[] rgsFilenames,
+					UInt32 nApplications,
+					[In] RM_UNIQUE_PROCESS[] rgApplications,
+					UInt32 nServices,
+					string[] rgsServiceNames);
+				[DllImport("rstrtmgr.dll", CharSet = CharSet.Auto)]
+				static extern int RmStartSession(out uint pSessionHandle, int dwSessionFlags, string strSessionKey);
+				[DllImport("rstrtmgr.dll")]
+				static extern int RmEndSession(uint pSessionHandle);
+				[DllImport("rstrtmgr.dll")]
+				static extern int RmGetList(uint dwSessionHandle,
+					out uint pnProcInfoNeeded,
+					ref uint pnProcInfo,
+					[In, Out] RM_PROCESS_INFO[] rgAffectedApps,
+					ref uint lpdwRebootReasons);
+				/// Find out what process(es) have a lock on the specified file.
+				/// <param name="path">Path of the file.</param>
+				/// <returns>Processes locking the file</returns>
+				/// http://msdn.microsoft.com/en-us/library/windows/desktop/aa373661(v=vs.85).aspx
+				/// http://wyupdate.googlecode.com/svn-history/r401/trunk/frmFilesInUse.cs (no copyright in code at time of viewing)
+				static public List<Process> WhoIsLocking(string path)
+				{
+					uint handle;
+					string key = Guid.NewGuid().ToString();
+					List<Process> processes = new List<Process>();
+					int res = RmStartSession(out handle, 0, key);
+					if (res != 0) throw new Exception("Could not begin restart session. Unable to determine file locker.");
+					try
+					{
+						const int ERROR_MORE_DATA = 234;
+						uint pnProcInfoNeeded = 0,
+						pnProcInfo = 0,
+						lpdwRebootReasons = RmRebootReasonNone;
+						// Just checking on one resource
+						string[] resources = new string[] { path };
+						res = RmRegisterResources(handle, (uint)resources.Length, resources, 0, null, 0, null);
+						if (res != 0) throw new Exception("Could not register resource.");
+						//Note: there's a race condition here -- the first call to RmGetList() returns
+						// the total number of process. However, when we call RmGetList() again to get
+						// the actual processes this number may have increased.
+						res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, null, ref lpdwRebootReasons);
+						if (res == ERROR_MORE_DATA)
+						{
+							// Create an array to store the process results
+							RM_PROCESS_INFO[] processInfo = new RM_PROCESS_INFO[pnProcInfoNeeded];
+							pnProcInfo = pnProcInfoNeeded;
+							// Get the list
+							res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, processInfo, ref lpdwRebootReasons);
+							if (res == 0)
+							{
+								processes = new List<Process>((int)pnProcInfo);
+								// Enumerate all of the results and add them to the 
+								// list to be returned
+								for (int i = 0; i < pnProcInfo; i++)
+								{
+									try
+									{
+										processes.Add(Process.GetProcessById(processInfo[i].Process.dwProcessId));
+									}
+									// catch the error -- in case the process is no longer running
+									catch (ArgumentException) { }
+								}
+							}
+							else throw new Exception("Could not list processes locking resource.");
+						}							else if (res != 0) throw new Exception("Could not list processes locking resource. Failed to get size of result.");
+					}
+					finally
+					{
+						RmEndSession(handle);
+					}
+						return processes;
+					}
+			}
+		}
+'@
+        if (-not ('MyCore.Utils.FileLockUtil' -as [type])) {
+            Add-Type -ReferencedAssemblies $ReferencedAssemblies -TypeDefinition $TypeDefinition
+        }
+        [MyCore.Utils.FileLockUtil]::WhoIsLocking($FilePath)
+    }
+   
 
 
     #run powershell as trusted installer credit : https://github.com/AveYo/LeanAndMean
@@ -255,6 +403,9 @@ foreach(`$err in `$Error){
     #get errors for files in use
     $errors = Get-Content "$env:ProgramData\error.txt" -Force | Select-String 'System.IO.IOException:' 
 
+
+    #if path is dir and -recurse do not compare to error list file name instead just kill all processes in the dir
+
     if ($errors -ne $null) {
         #extract file paths from errors
         $openFiles = @()
@@ -263,19 +414,27 @@ foreach(`$err in `$Error){
             $openFiles += $matches[1]  
         }
         #download handle app from sys internals to get which process is using the file
-        DownloadHandleApp
-        $handlePath = "$env:ProgramData\Handle\handle.exe"
-        #run handle
-        if ($type.Attributes -eq 'Directory') {
-            $output = & $handlePath $Path -accepteula
-        }
-        else {
-            $output = & $handlePath $dir -accepteula
-        }
+        #DownloadHandleApp
+        #$handlePath = "$env:ProgramData\Handle\handle.exe"
 
-        foreach ($line in $output) {
-            if ($line -like '*pid:*') {
-                foreach ($file in $openFiles) {
+       
+        $files = @()
+        $pids = @{}
+        #get each file if path has wildcard or is dir
+        $files = Get-ChildItem -Path $Path -Recurse -Force
+        foreach ($file in $files) {
+            #hastable with pid and file path
+            $pids[$file.FullName] = (Get-FileLockProcess -FilePath $file.FullName).Id
+        }
+        
+
+
+        foreach ($item in $pids.GetEnumerator()) {
+            $procId = $item.Value
+            $path = $item.Key
+            $command = "Stop-Process -id $procID -Force; Remove-Item -Path $path -Force"
+            RunAsTI powershell "-NoLogo -WindowStyle Hidden -Command $command" 
+            <#
                     #remove quotes
                     $file = $file -replace "'" , ' '
                     #check the path from error file with handle output
@@ -285,19 +444,20 @@ foreach(`$err in `$Error){
                         $procID = $matches[1]
                         $command = "Stop-Process -id $procID -Force"
                         RunAsTI powershell "-NoLogo -WindowStyle Hidden -Command $command" 
-                      
+                        Start-Sleep 1
                         #now try to remove file
+                        #kill and remove in one command
                         $command = "Remove-Item -Path $file -Force"
                         RunAsTI powershell "-NoLogo -WindowStyle Hidden -Command $command"
                     }
-                }
-            }
+                    #>
+            
         }
 
     }
 
     #cleanup error file
-    Remove-Item -Path "$env:ProgramData\error.txt" -Force -ErrorAction SilentlyContinue
+    #Remove-Item -Path "$env:ProgramData\error.txt" -Force -ErrorAction SilentlyContinue
 
     Start-Sleep 2
     #check that file / folder is deleted
